376fb060b2460704c6988597ed642dd342025ac0
Tekisatsu <ale.hartikainen@gmail.com>
2025-04-24 20:12:12 +0300

    barebones mökki and osoite classes

---
+++ Osoite.java
@@ -0,0 +1,38 @@
+public class Osoite {
+    String osoite;
+    String kaupunki;
+    String maa;
+    int zip;
+
+    public String getOsoite() {
+        return osoite;
+    }
+
+    public void setOsoite(String osoite) {
+        this.osoite = osoite;
+    }
+
+    public String getKaupunki() {
+        return kaupunki;
+    }
+
+    public void setKaupunki(String kaupunki) {
+        this.kaupunki = kaupunki;
+    }
+
+    public String getMaa() {
+        return maa;
+    }
+
+    public void setMaa(String maa) {
+        this.maa = maa;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+}
