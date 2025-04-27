0c9814bf68d183fe33fd5c0d36902a6c2ea735c0
Viljami01 <hiuka.musketit_7z@icloud.com>
2025-04-27 19:14:00 +0300

    Add files via upload

---
+++ Asiakas.java
@@ -0,0 +1,50 @@
+public class Asiakas {
+
+    private int id;
+    private String nimi;
+    private String sahkoposti;
+    private String puhelinnumero;
+    private Osoite osoite;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public String getSahkoposti() {
+        return sahkoposti;
+    }
+
+    public void setSahkoposti(String sahkoposti) {
+        this.sahkoposti = sahkoposti;
+    }
+
+    public String getPuhelinnumero() {
+        return puhelinnumero;
+    }
+
+    public void setPuhelinnumero(String puhelinnumero) {
+        this.puhelinnumero = puhelinnumero;
+    }
+
+    public Osoite getOsoite() {
+        return osoite;
+    }
+
+    public void setOsoite(Osoite osoite) {
+        this.osoite = osoite;
+    }
+
+}
