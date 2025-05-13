b0e0c21363159987a6ceb7790f780242b3ac193b
Viljami01 <hiuka.musketit_7z@icloud.com>
2025-05-13 16:25:07 +0300

    Javadoc-kommentit

--- TietokantaYhteysRaportti.java
+++ TietokantaYhteysRaportti.java
@@ -2,17 +2,31 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.math.BigDecimal;
 
-
+/**
+ * TietokantaYhteysRaportti-luokka sisältää kaikki metodit, joita tarvitaan käyttöliittymän ja
+ * tietokannan yhdistämiseen raporttien osalta.
+ */
 public class TietokantaYhteysRaportti {
 
     private String url = "jdbc:mysql://localhost:3306/mokkikodit?useSSL=false";
     private String kayttajanimi = "root";
     private String salasana = "tietokantaSalasana";
 
+    /**
+     * Luo yhteyden tietokantaa.
+     * @return tietokantayhteys
+     * @throws SQLException mikäli yhteyden luonti epäonnistuu
+     */
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, kayttajanimi, salasana);
     }
 
+    /**
+     * Hakee varausten määrän tietokannasta halutulta päivämääräväliltä.
+     * @param alku haluttu alkupäivämäärä
+     * @param loppu haluttu päättymispäivämäärä
+     * @return 0, mikäli virhe varausmäärän haussa
+     */
     public int haeVaraustenMaara(LocalDate alku, LocalDate loppu) {
         String sql = "SELECT COUNT(*) FROM VARAUS WHERE Aloituspaivamaara >= ? AND Paattymispaivamaara <= ?";
         try (Connection conn = getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -21,11 +35,17 @@ public class TietokantaYhteysRaportti {
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) return rs.getInt(1);
         } catch (SQLException e) {
-            System.err.println("Virhe keskimääräisen keston haussa: " + e.getMessage());
+            System.err.println("Virhe varausmäärän haussa: " + e.getMessage());
         }
         return 0;
     }
 
+    /**
+     * Metodi varauksiin liitettyjen asiakkaiden hakuun halutulta päivämääräväliltä.
+     * @param alku haluttu alkupäivämäärä
+     * @param loppu haluttu päättymispäivämäärä
+     * @return 0, mikäli virhe asiakasmäärän haussa
+     */
     public int haeAsiakasmaara(LocalDate alku, LocalDate loppu) {
         String sql = """
                 SELECT COUNT(DISTINCT Asiakas_ID)
@@ -43,6 +63,12 @@ public class TietokantaYhteysRaportti {
         return 0;
     }
 
+    /**
+     * Metodi kokonaislaskutuksen hakuun tietyltä aikaväliltä.
+     * @param alku haluttu alkupäivämäärä
+     * @param loppu haluttu päättymispäivämäärä
+     * @return 0, mikäli virhe laskutuksen haussa
+     */
     public BigDecimal haeKokonaislaskutus(LocalDate alku, LocalDate loppu) {
         String sql = """
                 SELECT SUM(LASKU.Maara)
@@ -61,6 +87,12 @@ public class TietokantaYhteysRaportti {
         return BigDecimal.ZERO;
     }
 
+    /**
+     * Metodi hakee suosituimman mökin varauksien määrän perusteella tietyltä aikaväliltä.
+     * @param alku tarkasteluvälin alkamispäivämäärä
+     * @param loppu tarkasteluvälin päättymispäivämäärä
+     * @return "Ei tietoa", mikäli virhe suosituimman mökin haussa
+     */
     public String haeSuosituinMokki(LocalDate alku, LocalDate loppu) {
         String sql = """
                 SELECT MOKKI.Nimi, COUNT(*) AS varauksia
@@ -82,6 +114,12 @@ public class TietokantaYhteysRaportti {
         return "Ei tietoa";
     }
 
+    /**
+     * Metodi hakee varauksien keskimääräisen keston tietyltä aikaväliltä.
+     * @param alku tarkasteluvälin alkamispäivämäärä
+     * @param loppu tarkasteluvälin päättymispäivämäärä
+     * @return 0, mikäli virhe keskimääräisen keston haussa
+     */
     public int haeKeskimaarainenKesto(LocalDate alku, LocalDate loppu) {
         String sql = """
                 SELECT AVG(DATEDIFF(Paattymispaivamaara, Aloituspaivamaara))
